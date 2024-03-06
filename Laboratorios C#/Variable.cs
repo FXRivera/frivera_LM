9c9a921909a48a090e4a53672fe4cdf496ee9afc
Francis Rivera <francxavier2005@gmail.com>
2024-03-05 21:08:33 -0400

    nuevos files

---
+++ Laboratorios C#/Variable.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program{
+    static void Main(){
+        int myVariable = 10;
+        const int myConstant = 20;
+            Console.WriteLine("Valor de mi variable: " + myVariable);
+            Console.WriteLine("Valor de mi constante: " +myConstant);
+    }
+}
