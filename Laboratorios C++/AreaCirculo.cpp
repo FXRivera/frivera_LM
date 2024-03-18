38248ad1eb3ccf08c3c2a51b566022c7b65a11d7
Francis Rivera <francxavier2005@gmail.com>
2024-03-18 09:23:02 -0400

    updated

---
+++ Laboratorios C++/AreaCirculo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //definicion de variables
+    double area;
+    double radio;
+    const double PI = 3.14; //Identificador constante
+
+    cout <<"Ingrese la medida del radio: ";
+    cin >> radio;
+
+    area = PI * radio * radio;
+
+    cout <<"El área del círculo es: "<<area <<endl;
+
+    return 0;
+
+}
