a35670ec2092b085200aeb4d576c101b9cdca783
Francis Rivera <francxavier2005@gmail.com>
2024-04-14 14:53:07 -0400

    update

--- Laboratorios C++/Nombre10VecesFor/Nombre10VecesFor.cpp
+++ Laboratorios C++/Nombre10VecesFor/Nombre10VecesFor.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 
 int main(){
+    //definición de variables
     string nombre;
     int i;
 
+        //le pide al usuario que ingrese su nombre
         cout << "Ingrese su nombre: ";
         cin >> nombre;
 
+    //la i se suma hasta 10
     for(i = 1; i <= 10; i++){
+        //imprime el número de veces hasta donde sumó
         cout <<"Hola "<< nombre <<"!"<<endl;
     }
-
-
     return 0;
 }
--- Laboratorios Java/Nombre10VecesFor/src/Nombre10VecesFor.java
+++ Laboratorios Java/Nombre10VecesFor/src/Nombre10VecesFor.java
@@ -4,10 +4,16 @@ public class Nombre10VecesFor {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
+        //le pide al usuario que ingrese su nombre
         System.out.print("Igrese su nombre: ");
+
+        //ell nombre ingresado se almacena
         String nombre = scanner.nextLine();
 
+        //la i se suma hasta 10
         for(int i = 1; i <= 10; i++){
+
+            //imprime el número de veces hasta donde sumó
             System.out.println("Hola "+ nombre +"!");
         }
         scanner.close();
