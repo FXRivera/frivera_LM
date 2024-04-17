20b8ee2459cee3cda538541423b229ccf1f387d2
Francis Rivera <francxavier2005@gmail.com>
2024-04-17 10:00:46 -0400

    examen ejemplo

---
+++ Laboratorios Java/ExamenPrueba/src/ExamenPrueba.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ExamenPrueba {
+    public static void main(String[]args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        double sumaCalificacion = 0;
+        double promedio;
+        double calificacion;
+
+        for(int i = 1; i <= 3; i++){
+            System.out.print("Ingrese calificación " + i + ": ");
+            calificacion = scanner.nextDouble();
+
+            sumaCalificacion += calificacion;
+        }
+        promedio = sumaCalificacion/3;
+
+        System.out.println("El promedio de las calificaciones es: " + promedio);
+
+        scanner.close();
+    }
+}
